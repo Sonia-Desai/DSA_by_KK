bdccaee79f1e7ee3fe9e4098a193318e76e4e9da
Sonia Desai <soniaprakash24@gmail.com>
2025-11-01 18:17:47 +0530

    added for loop in p2

--- src/com/selfstudy/karrays/p2.java
+++ src/com/selfstudy/karrays/p2.java
@@ -1,5 +1,18 @@
 package com.selfstudy.karrays;
 
+import java.util.Scanner;
+
 public class p2 {
+    public static void main(String[] args) {
+        int arr [] = new int[55];
+        int a = 10;
+
+        for(int i = 0 ; i<arr.length;i++){
+            System.out.print(a+" ");
+            a++;
+
+        }
+
+    }
 
 }
