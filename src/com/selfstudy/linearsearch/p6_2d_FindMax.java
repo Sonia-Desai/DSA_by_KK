7c15ed79486b992ab9eff00a68934bbbabccd7ab
Sonia Desai <soniaprakash24@gmail.com>
2025-12-30 12:22:55 +0530

    finding MAX in 2d array

--- src/com/selfstudy/linearsearch/p6_2d_FindMax.java
+++ src/com/selfstudy/linearsearch/p6_2d_FindMax.java
@@ -1,4 +1,44 @@
 package com.selfstudy.linearsearch;
 
+import java.util.Arrays;
+
 public class p6_2d_FindMax {
+    public static void main(String[] args) {
+        int [][] arr = {
+                {
+                        10,20,30,40
+                },{
+                50,60,70,80
+        },{12,24,36,48},{
+                23,24,25,26,97
+        }
+        };
+
+      int ans = findSearch(arr);
+        System.out.println(ans);
+
+
+
+
+    }
+
+    public static int  findSearch(int[][] arr ) {
+            int result = arr[0][0];
+        for(int i = 1; i< arr.length ; i++){
+            for(int j = 1 ; j<arr[i].length ; j++){
+              if(arr[i][j]> result){
+                  result = arr[i][j];
+
+              }
+
+
+            }
+        }
+
+        return result;
+
+
+
+
+    }
 }
